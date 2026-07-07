f9f9878291865aae311d3d1db00b2aaa051389c5
sakhile <sakhiledabula0444@gmail.com>
2026-07-07 13:06:44 +0200

    employee

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("============EMPLOYEE PAYROLL=================");
+        System.out.println("Add employee");
+        System.out.println("view employee");
+        System.out.println("calculate payroll");
+        System.out.println("remove employee");
+        System.out.println("exit");
+
+
+
+        class employee{
+            private String employeeID;
+            private String name;
+            private String surname;
+            private int salary;
+        }
+
+        class developer{
+           private String developerID;
+            private String name;
+            private String surname;
+            private int salary;
+
+        }
+
+        class Managers {
+            private String ManagersID;
+            private String name;
+            private String surname;
+            private int salary;
+
+
+
+            class intern{
+                private String InternID;
+                private String name;
+                private String surname;
+                private int salary;
+
+            }
+
+        }
+
+
+
+
+            }
+        }
+
+
