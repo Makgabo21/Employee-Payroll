41e026b2a0e73dd63e1e42a37b29de3946cc2812
Charlotte <cnkuna920@gmail.com>
2026-07-07 16:29:06 +0200

    new commit

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,14 @@ public class Main {
             System.out.println("\n===== EMPLOYEE PAYROLL =====");
             System.out.println("1. Add Employee");
             System.out.println("2. View Employees");
+            //Charlotte:  remove and exit
+            System.out.println("5. Remove Employee");
+            System.out.println("6. Exit");
             System.out.print("Choice: ");
 
             // Check that the user entered a number
             if (!input.hasNextInt()) {
-                System.out.println("Invalid input! Please enter 1 or 2.");
-                input.next(); // Remove the invalid input
+                System.out.println("Invalid input! Please enter a number between 1 and 6.");                input.next(); // Remove the invalid input
                 continue;
             }
 
@@ -36,6 +38,20 @@ public class Main {
                 case 2:
                     ViewEmployee.viewEmployees(employees);
                     break;
+
+                // ================= CHARLOTTE - REMOVE EMPLOYEE & EXIT START =================
+
+                case 5:
+                    RemoveEmployee.removeEmployee(employees);
+                    break;
+
+                case 6:
+                    System.out.println("\nThank you for using Employee Payroll System.");
+                    input.close();
+                    System.exit(0);
+                    break;
+
+// ================= CHARLOTTE - REMOVE EMPLOYEE & EXIT END =================
 
                 default:
                     System.out.println("Invalid option!");
