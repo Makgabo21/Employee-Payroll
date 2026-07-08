52326e1aa0702c4daf9757a78a7b50057c109053
Sibongile <sibongilearathokwane@gmail.com>
2026-07-08 13:49:54 +0200

    pulling develop

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,6 @@ public class Main {
             System.out.println("\n===== EMPLOYEE PAYROLL =====");
             System.out.println("1. Add Employee");
             System.out.println("2. View Employees");
-            System.out.println("3. Search Employee");
-            System.out.println("4. Calculate Payroll");
             System.out.println("5. Remove Employee");
             System.out.println("6. Exit");
             System.out.print("Choice: ");
